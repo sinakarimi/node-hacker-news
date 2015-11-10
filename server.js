5ff9d0f19242e8cef029ae3fc2d74dd327dbccaa
Sina Karimi <creamycode@gmail.com>
2015-11-10 20:55:01 +1100

    Added application logging - Also added app module path so we can reference local modules more simply

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+require('app-module-path').addPath(__dirname);
+
 var commander = require('commander');
+var config = require('lib/config');
+var logger = require('lib/logger');
 var app = require('./app');
 
 // Set the command line arguments available
@@ -7,9 +11,9 @@ commander
 	.option('-p, --port <port>', 'Port to listen on', parseInt)
 	.parse(process.argv);
 
-var port = commander.port || 8080;
+var port = commander.port || config.core.port;
 app.listen(port);
-
+logger.info('Node Hacker News server listening on port ' + port);
 // Uncaught exception handler.
 process.addListener('uncaughtException', function(err) {
 	logger.uncaught('Uncaught error in server.js', { err:err, stack: err.stack });
