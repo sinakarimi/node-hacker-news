07e9e444324334ce07253782db89a6c72053303c
Sina Karimi <creamycode@gmail.com>
2015-11-11 22:23:28 +1100

    e2e test for listing an article

---
+++ src/article/test/e2e/list-test.js
@@ -0,0 +1,24 @@
+var should = require('should');
+var request = require('supertest')(test.app);
+
+describe('Article:e2e:list', function() {
+	var url = '/';
+
+	it('should get a list of articles sorted from newest to oldest', function(done) {
+		request
+			.get(url)
+			.expect(200, function(err, res) {
+				should.not.exist(err);
+				should.exist(res.body);
+
+				res.body.should.be.a.Array;
+				res.body.should.be.instanceof(Array).and.have.lengthOf(Object.keys(test.fixtures.article).length)
+				should.exist(res.body[0]._id);
+
+				// Ensure it's sorted as newest article first
+				res.body[0].objectID.should.equal(test.fixtures.article.newDate.objectID);
+
+				done();
+			});
+	});
+});
--- test/globals.js
+++ test/globals.js
@@ -1,6 +1,9 @@
+require('app-module-path').addPath(__dirname + '/../');
+
 var path = require('path');
 
 var test = {
+	app: require('../app'),
 	fixtures: require('./fixtures'),
 };
 
