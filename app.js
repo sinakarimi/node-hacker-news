7ba51a0784313da6204abd18839bcb376fdf2a5b
Sina Karimi <creamycode@gmail.com>
2015-11-12 17:25:23 +1100

    Added article list controller + base view

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ require('lib/mongoose-connection');
 var express = require('express');
 var http = require('http');
 var logger = require('lib/logger');
+var jade = require('jade');
 
 var app = express();
+// app.use(express.bodyParser());
+
+// Set the default view engine to Jade.
+app.set('views', __dirname + '/src')
+app.set('view engine', 'jade');
 
 // Attach router
 app.use(require('lib/router'));
--- src/article/controller/index.js
+++ src/article/controller/index.js
@@ -1,9 +1,17 @@
 var _ = require('lodash');
+var Article = require('src/article/model');
 
 var controller = {
 	list: function(req, res, next) {
-		// @TODO: Implement this function
-		res.send('list');
+		Article.find()
+		.sort('-created_at')
+		.exec(function(err, articles) {
+			if (err) {
+				return next(err);
+			}
+
+			controller.render(req, res, 'article/view/list', articles);
+		});
 	},
 	delete: function(req, res, next) {
 		// @TODO: Implement this function
